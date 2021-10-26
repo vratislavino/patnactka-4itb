d66981ef4028c69c08e0e54fcaa7a439021ef9b0
vratislavino <vratislavino@gmail.com>
2021-10-26 19:28:29 +0200

    Prvni dvouhodinovka

---
+++ Hra 15/Segment.Designer.cs
@@ -0,0 +1,60 @@
+﻿
+namespace Hra_15
+{
+    partial class Segment
+    {
+        /// <summary> 
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód vygenerovaný pomocí Návrháře komponent
+
+        /// <summary> 
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent() {
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(197, 200);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "1";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Segment
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.label1);
+            this.Name = "Segment";
+            this.Size = new System.Drawing.Size(200, 200);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ Hra 15/Segment.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hra_15
+{
+    public partial class Segment : UserControl
+    {
+        public event Action<Segment> PositionChangeRequested;
+
+        int cislo;
+        public Segment(int a):this() {
+            cislo = a;
+            label1.Text = cislo.ToString();
+
+
+        }
+
+
+        public Segment() {
+
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e) {
+            PositionChangeRequested?.Invoke(this);
+        }
+    }
+}
